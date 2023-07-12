68172541cfd422875604f44c0e893c88dc750c85
Richard Zampieri <br.developer@gmail.com>
2023-07-11 21:53:57 -0700

    fix: add config output

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
   // basePath: '/expressots-site',
   /* images: {
     domains: ['localhost', 'expressots.github.io'],
