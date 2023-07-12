34528e60381fed30585a142dd51821d31218972a
ArielBetti <ariel.betti@gmail.com>
2023-07-12 04:48:57 -0300

    fix: asset prefix

--- next.config.js
+++ next.config.js
@@ -4,11 +4,7 @@ const nextConfig = {
   output: 'export',
   // basePath: '/expressots-site',
   images: { unoptimized: true },
-  assetPrefix: '/expressots-site',
-  publicRuntimeConfig: {
-    basePath: '/expressots-site',
-  },
-  basePath: '/expressots-site',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/expressots-site' : '',
 }
 
 module.exports = nextConfig
