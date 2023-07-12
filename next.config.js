42ff646aebd7a400004aea8c34b36791872a5a5b
ArielBetti <ariel.betti@gmail.com>
2023-07-12 11:38:19 -0300

    fix: next config listen phase

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  output: 'export',
-  // basePath: '/expressots-site',
-  images: { unoptimized: true },
-  assetPrefix: '/expressots-site',
-  basePath: '/expressots-site',
+const { PHASE_PRODUCTION_BUILD } = require('next/dist/shared/lib/constants')
+
+const nextConfig = (phase) => {
+  const isProduction = phase === PHASE_PRODUCTION_BUILD
+
+  return {
+    output: 'export',
+    // basePath: '/expressots-site',
+    images: { unoptimized: true },
+    assetPrefix: isProduction ? 'https://expressots.github.io/expressots-site' : '',
+  }
 }
 
 module.exports = nextConfig
