a05207ac18dad9ad42eeaec5bd4f62a849c434d2
Felipe Fontana <felipefontanaf@gmail.com>
2023-07-12 22:11:49 -0400

    feat: implement team page - run prettier header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,30 +6,30 @@ import { NavMobile } from './nav-mobile'
 
 export const Header = () => {
   return (
-    <header className="fixed z-50 flex items-center justify-between w-full py-8 border border-b shadow-md h-14 border-neutral-900 bg-neutral-950 bg-neutral-950/90 backdrop-blur-sm lg:inset-0 lg:top-0">
-      <div className="container flex items-center justify-between px-4 mx-auto">
+    <header className="fixed z-50 flex h-14 w-full items-center justify-between border border-b border-neutral-900 bg-neutral-950 bg-neutral-950/90 py-8 shadow-md backdrop-blur-sm lg:inset-0 lg:top-0">
+      <div className="container mx-auto flex items-center justify-between px-4">
         <Link
           href="/"
-          className="flex items-center justify-start gap-3 border border-transparent rounded-full overlow-hidden group hover:animate-ascend"
+          className="overlow-hidden group flex items-center justify-start gap-3 rounded-full border border-transparent hover:animate-ascend"
         >
           <Image width={50} height={50} src="./assets/logos/logo-circular-dark.svg" alt="" />
         </Link>
         <div className="flex items-center gap-4 max-lg:hidden">
           <ul className="flex items-center justify-end gap-5">
             <MenuLi href="https://doc.expresso-ts.com/docs/hello">
-              <IconBook className="w-5 h-5 mr-2" aria-hidden="true" />
+              <IconBook className="mr-2 h-5 w-5" aria-hidden="true" />
               Documentation
             </MenuLi>
             <MenuLi href="https://doc.expresso-ts.com/docs/hello">
-              <IconBriefcase className="w-5 h-5 mr-2" aria-hidden="true" />
+              <IconBriefcase className="mr-2 h-5 w-5" aria-hidden="true" />
               Showcases
             </MenuLi>
             <MenuLi href="/team">
-              <IconUsersGroup className="w-5 h-5 mr-2" aria-hidden="true" />
+              <IconUsersGroup className="mr-2 h-5 w-5" aria-hidden="true" />
               Team
             </MenuLi>
             <MenuLi href="https://github.com/expressots/expressots">
-              <IconBrandGithub className="w-5 h-5 mr-2" aria-hidden="true" />
+              <IconBrandGithub className="mr-2 h-5 w-5" aria-hidden="true" />
               Github
             </MenuLi>
           </ul>
