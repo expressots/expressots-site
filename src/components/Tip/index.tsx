1e0f105402b0382da67cb1d9a8056a9011b78845
Felipe Fontana <felipefontanaf@gmail.com>
2023-07-13 02:13:35 -0400

    fix: styling tips / footer /home

--- src/components/Tip/index.tsx
+++ src/components/Tip/index.tsx
@@ -13,7 +13,7 @@ const Tip = forwardRef<HTMLDivElement, TTipProps>(
     return (
       <div
         className={clsxm(
-          'flex min-h-[30px] min-w-max items-center justify-between gap-3 overflow-hidden rounded-full border border-neutral-2 bg-neutral-1/60 px-2 shadow-md backdrop-blur-sm',
+          'flex min-h-[30px] w-max items-center justify-between gap-3 overflow-hidden rounded-full border border-neutral-2 bg-neutral-1/60 px-2 shadow-md backdrop-blur-sm',
           className,
         )}
         ref={ref}
@@ -22,9 +22,9 @@ const Tip = forwardRef<HTMLDivElement, TTipProps>(
         <div className="flex items-center justify-start">{children}</div>
         <button
           onClick={() => setIsVisible(false)}
-          className="flex h-4 w-4 items-center justify-center overflow-hidden rounded-full border border-transparent transition-colors hover:border-red-500 hover:bg-red-950 hover:text-red-500"
+          className="flex items-center justify-center w-4 h-4 overflow-hidden transition-colors border border-transparent rounded-full hover:border-red-500 hover:bg-red-950 hover:text-red-500"
         >
-          <IconX className="h-3 w-3" />
+          <IconX className="w-3 h-3" />
         </button>
       </div>
     )
--- src/components/ui/link-button.tsx
+++ src/components/ui/link-button.tsx
@@ -8,11 +8,12 @@ type Props = {
 
 export const LinkButton = ({ children, href }: Props) => {
   return (
-    <button className="pattern-green-oil flex items-center justify-center overflow-hidden rounded-md border border-base-6 bg-fixed duration-100 ease-out hover:bg-base-6 hover:text-base-12 active:scale-95">
+    <button className="flex items-center justify-center overflow-hidden duration-100 ease-out bg-fixed border rounded-md pattern-green-oil border-base-6 hover:bg-base-6 hover:text-base-12 active:scale-95">
       <Link
+        target="_blank"
         href={href}
         draggable={false}
-        className="flex h-14 select-none items-center justify-center bg-base-12/90 px-6 py-3 text-lg font-bold text-base-5 duration-100 ease-out hover:bg-transparent hover:text-base-12"
+        className="flex items-center justify-center px-6 py-3 text-lg font-bold duration-100 ease-out select-none h-14 bg-base-12/90 text-base-5 hover:bg-transparent hover:text-base-12"
       >
         {children}
       </Link>
