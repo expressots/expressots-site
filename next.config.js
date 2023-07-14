28ed46122afa756284fee972052ef50067629124
ArielBetti <ariel.betti@gmail.com>
2023-07-14 16:24:40 -0300

    downgraded nextjs to version 13.2.3 to fix app dir bug

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = (phase) => {
   const isProduction = phase === PHASE_PRODUCTION_BUILD
 
   return {
+    experimental: {
+      appDir: true,
+    },
     output: 'export',
     // basePath: '/expressots-site',
     images: { unoptimized: true },
