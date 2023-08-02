035f97535dae397fbb0c52b0d14b82785a900afd
Felipe Fontana <felipefontanaf@gmail.com>
2023-08-02 15:56:04 -0400

    fix: broken link #9

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,20 +5,20 @@ import Link from 'next/link'
 const Footer = () => {
   const Anchor = ({ link, name }: { link: string; name: string }) => {
     return (
-      <Link className="flex h-12 items-center hover:text-base-6" target="_blank" href={link}>
+      <Link className="flex items-center h-12 hover:text-base-6" target="_blank" href={link}>
         {name}
       </Link>
     )
   }
 
   return (
-    <footer className="border-t border-neutral-1 bg-grid bg-cover bg-no-repeat">
+    <footer className="bg-no-repeat bg-cover border-t border-neutral-1 bg-grid">
       <div className="min-h-[250px] bg-neutral-950/90 py-5">
-        <Container className="flex h-full flex-col items-start justify-center gap-10 md:flex-row">
+        <Container className="flex flex-col items-start justify-center h-full gap-10 md:flex-row">
           <ul className="flex flex-col font-thin">
             <li className="pb-3 font-semibold">Docs</li>
             <li>
-              <Anchor name="Tutorial" link="https://doc.expresso-ts.com/docs/tutorials/overview" />
+              <Anchor name="Tutorial" link="https://doc.expresso-ts.com/docs/category/tutorials" />
             </li>
           </ul>
           <ul className="font-thin">
@@ -51,7 +51,7 @@ const Footer = () => {
 
         {/* Additional copyright information */}
         <Container>
-          <div className="py-5 text-center text-sm">
+          <div className="py-5 text-sm text-center">
             <p>Released under the MIT License</p>
             <p>Official ExpressoTS Site</p>
             <p>
