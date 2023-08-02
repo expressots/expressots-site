1f92935679481f015ec107051c66bd21d0c9d48b
Felipe Fontana <felipefontanaf@gmail.com>
2023-08-02 13:58:23 -0400

    feat: product hunt badge

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -14,17 +14,17 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex h-full min-h-screen flex-col items-center justify-center overflow-auto">
-      <div className="w-full border border-b border-neutral-1 bg-grid bg-cover bg-right-top bg-no-repeat">
+    <main className="flex flex-col items-center justify-center h-full min-h-screen overflow-auto">
+      <div className="w-full bg-right-top bg-no-repeat bg-cover border border-b border-neutral-1 bg-grid">
         <div className="bg-black/80 backdrop-blur-sm">
           <Container>
             <div className="relative flex min-h-[calc(85vh)] w-full flex-col items-center justify-center">
-              <div className="absolute left-1/2 top-24 z-40 -translate-x-1/2 -translate-y-1/2 transform">
-                <Tip className="animate-showup text-sm text-neutral-6 transition-shadow">
+              <div className="absolute z-40 transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-24">
+                <Tip className="text-sm transition-shadow animate-showup text-neutral-6">
                   🎉 Production ready since v1.5.0
                 </Tip>
               </div>
-              <div className="mt-32 flex h-full flex-col items-center justify-center gap-4 sm:mt-0">
+              <div className="flex flex-col items-center justify-center h-full gap-4 mt-32 sm:mt-0">
                 <Image
                   width={100}
                   height={100}
@@ -34,12 +34,24 @@ export default function Home() {
                 <h1 className="text-center text-6xl text-[clamp(48px_5vw_72px)] font-bold">
                   ExpressoTS
                 </h1>
-                <p className="text-center text-xl text-neutral-10">
+                <p className="text-xl text-center text-neutral-10">
                   The Developer-Friendly TypeScript Framework for Server-Side Applications
                 </p>
-                <div className="mt-10 flex flex-col items-center justify-center gap-4 md:flex-row">
+                <div className="flex flex-col items-center justify-center gap-4 mt-10 md:flex-row">
                   <LinkButton href="https://doc.expresso-ts.com/docs/hello">Get Started</LinkButton>
                   <CopyInstalation code="npm i -g @expressots/cli" />
+                </div>
+                <div className="flex">
+                  <a
+                    href="https://www.producthunt.com/posts/expressots?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-expressots"
+                    target="_blank"
+                  >
+                    <img
+                      src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=407781&theme=light"
+                      alt="ExpressoTS - Developer&#0045;Friendly&#0032;TypeScript&#0032;Framework&#0032;for&#0032;Server&#0045;Side&#0032;Apps | Product Hunt"
+                      className="w-64 h-12"
+                    />
+                  </a>
                 </div>
               </div>
             </div>
@@ -48,11 +60,11 @@ export default function Home() {
       </div>
       <div className="pattern-green-oil relative min-h-[200px] w-full bg-fixed">
         <div className="bg-black/90">
-          <Container className="flex h-full flex-col items-center justify-center gap-5 py-10 text-center md:flex-row">
+          <Container className="flex flex-col items-center justify-center h-full gap-5 py-10 text-center md:flex-row">
             <InfoCard
               header={
                 <>
-                  <IconApps className="mr-2 h-5 w-5" aria-hidden="true" />
+                  <IconApps className="w-5 h-5 mr-2" aria-hidden="true" />
                   <p className="font-semibold">Easy to use</p>
                 </>
               }
@@ -61,7 +73,7 @@ export default function Home() {
             <InfoCard
               header={
                 <>
-                  <IconViewfinder className="mr-2 h-5 w-5" aria-hidden="true" />
+                  <IconViewfinder className="w-5 h-5 mr-2" aria-hidden="true" />
                   <p className="font-semibold">Focus on What Matters</p>
                 </>
               }
@@ -70,7 +82,7 @@ export default function Home() {
             <InfoCard
               header={
                 <>
-                  <IconPackages className="mr-2 h-5 w-5" aria-hidden="true" />
+                  <IconPackages className="w-5 h-5 mr-2" aria-hidden="true" />
                   <p className="font-semibold">Extensible</p>
                 </>
               }
@@ -81,9 +93,9 @@ export default function Home() {
       </div>
       <div className="pattern-green-oil relative min-h-[200px] w-full bg-fixed">
         <div className="bg-black/90">
-          <Container className="flex h-full flex-col items-center gap-5 py-10 text-center">
-            <h2 className="m-0 p-0 text-4xl text-base-5">Current Partners</h2>
-            <p className="m-0 p-0 font-normal">
+          <Container className="flex flex-col items-center h-full gap-5 py-10 text-center">
+            <h2 className="p-0 m-0 text-4xl text-base-5">Current Partners</h2>
+            <p className="p-0 m-0 font-normal">
               ExpressoTS is an MIT-licensed open-source project. Consider supporting us!
             </p>
             <div className="py-10">
@@ -93,10 +105,10 @@ export default function Home() {
         </div>
       </div>
       <div className="pattern-green-oil relative hidden min-h-[200px] w-full border-t border-neutral-2 bg-fixed shadow-[0px_-10px_50px] shadow-base-12/80 md:flex">
-        <div className="w-full bg-neutral-950/80 py-10">
-          <Container className="flex h-full flex-col items-center gap-5 py-10 text-center">
-            <h2 className="m-0 p-0 text-4xl text-base-4">Live preview</h2>
-            <p className="m-0 p-0 font-normal text-neutral-12">
+        <div className="w-full py-10 bg-neutral-950/80">
+          <Container className="flex flex-col items-center h-full gap-5 py-10 text-center">
+            <h2 className="p-0 m-0 text-4xl text-base-4">Live preview</h2>
+            <p className="p-0 m-0 font-normal text-neutral-12">
               See how ExpressoTS look like without leaving your browser.
             </p>
             <div className="w-full overflow-hidden rounded-md">
