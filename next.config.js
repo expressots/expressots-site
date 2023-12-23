0a9ac89c9d8cd182b2f7058f2a90dac79000cb7d
Richard Zampieri <br.developer@gmail.com>
2023-12-22 17:36:03 -0800

    removed output config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,10 @@
 
 const { PHASE_PRODUCTION_BUILD } = require('next/dist/shared/lib/constants')
 
-const nextConfig = phase => {
+const nextConfig = (phase) => {
   const isProduction = phase === PHASE_PRODUCTION_BUILD
 
   return {
-    output: 'export',
     // basePath: '/expressots-site',
     images: { unoptimized: true },
     assetPrefix: isProduction ? 'https://expresso-ts.com' : '',
