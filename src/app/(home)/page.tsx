3447c7215948b044557cd9a5971c64d04c7cd77c
Richard Zampieri <br.developer@gmail.com>
2024-03-31 05:52:20 -0700

    update: version to 2.10.0

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
             <div className="relative flex min-h-[calc(85vh)] w-full flex-col items-center justify-center">
               <div className="absolute left-1/2 top-24 z-40 -translate-x-1/2 -translate-y-1/2 transform">
                 <Tip className="animate-showup text-sm text-neutral-6 transition-shadow">
-                  🎉 Production version v2.9.0
+                  🎉 Production version v2.10.0
                 </Tip>
               </div>
               <div className="mt-32 flex h-full flex-col items-center justify-center gap-4 sm:mt-0">
